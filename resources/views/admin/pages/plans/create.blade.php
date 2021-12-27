26cb74b99202a187f9cfd099bc0462dce3367e8e
AllanRanier <allanranier18@gmail.com>
2021-12-27 13:02:25 -0300

    first commit

---
+++ resources/views/admin/pages/plans/create.blade.php
@@ -0,0 +1,15 @@
+@extends('adminlte::page')
+
+@section('title,', 'Cadastrar Novo Plano')
+
+@section('content_header')
+    <h1>Planos</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+
+        </div>
+    </div>
+@endsection
